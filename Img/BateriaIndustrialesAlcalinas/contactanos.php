1b62784c69d2f9a634f68d0367e4b04d44d784ad
Jose Salvador Nuño Toledo <salvadorjnt@gmail.com>
2017-07-26 20:26:28 -0500

    Imagenes agregadas
    Se agregaron logotipos e imagenes faltantes

---
+++ Img/BateriaIndustrialesAlcalinas/contactanos.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>SIEYE-SISTEMAS DE ILUMINACIÓN EMERGENCIA Y ENERGIA</title>
+<style type="text/css">
+.auto-style1 {
+	border-style: solid;
+	border-width: 10px;
+}
+.auto-style2 {
+	text-align: center;
+}
+.auto-style3 {
+	border-left-style: solid;
+	border-left-width: 1px;
+	border-right: 1px solid #FFFFFF;
+	border-top-style: solid;
+	border-top-width: 1px;
+	border-bottom: 1px solid #FFFFFF;
+}
+.auto-style8 {
+	font-family: "Script MT Bold";
+	font-size: x-large;
+	color: #800000;
+}
+.Estilo3 {font-family: "Script MT Bold"; color: #990000; }
+.Estilo5 {font-size: 18px}
+.Estilo2 {color: #800000}
+.Estilo1 {font-size: 36px}
+.Estilo2 {font-family: "Script MT Bold"}
+.Estilo1 {color: #990000}
+.Estilo4 {color: #800000; font-size: 18px; }
+</style>
+</head>
+
+<body bgcolor="#FFFFCC">
+<table bgcolor="#ffffcc" align="center" class="auto-style1" style="width: 100%">
+
+<tbody>
+
+<tr bgcolor="#ffffff">
+
+<td width="1272" style="height: 160px">
+
+<div><img src="web_images/barra.gif" alt="741" width="100%" height="9" />
+
+<p class="auto-style2"><marquee behavior="alternate"><img src="web_images/imagen1.png" alt="logo" width="96" height="91" align="middle" /><span class="Estilo1">&nbsp;&nbsp;&nbsp;<img style="vertical-align: middle;" src="web_images/untitled-2_clip_image001.gif" alt="SIEYE" width="244" height="73" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-family: Cooper Black; font-size: large;">SISTEMAS DE ILUMINACION EMERGENCIA y ENERGIA</marquee> </span></span></p>
+
+</div>
+
+<p><img src="web_images/barra.gif" alt="741" width="100%" height="8" /></p>
+
+</td>
+
+</tr>
+
+<tr>
+
+<td>
+
+<div>
+
+<p><img src="web_images/colores.gif" alt="linea" width="100%" height="1" /></p>
+
+<p align="center"><em><strong><span style="font-size: small;">PANUCO N&deg; 5, COL, IGNACIO LOPEZ RAYON C.P. 52986, ATIZAPAN EDO. DE MEX. TELS. 01 (55) 53 62 30 81 / 22 91</span></strong></em></p>
+
+<p><img src="web_images/colores.gif" alt="linea" width="100%" height="1" /></p>
+
+</div>
+
+</td>
+
+</tr>
+
+<tr>
+
+<td>
+
+<div style="text-align: center;"><span style="font-family: Script MT Bold; font-size: small;"><img src="web_images/imagen1.png" alt="logo" width="96" height="91" align="middle" /></span><span class="auto-style8">Contactanos:</span></div>
+
+</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+
+<table align="center" style="width: 80%" bgcolor="#FFFFFF">
+	<tr>
+		<td class="auto-style2"><a href="home.html"><img src="web_images/imagen1.png" border="0" alt="SIEYE" width="57" height="56" /></a><br />
+		<br />
+		<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="108" height="28" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0">
+
+<param name="quality" value="high" />
+
+<param name="src" value="web_flash/button9.swf" /><embed type="application/x-shockwave-flash" width="108" height="28" src="web_flash/button9.swf" quality="high"></embed>
+
+</object>
+
+		</td>
+		<td class="auto-style2" style="width: 407px">
+		<label>
+		<a href="productos.html"><img border="0" alt="productos" width="151" height="84" src="c_volt_batteries_md_wht.gif" /></a></label><br />
+		<br />
+		<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="123" height="30" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0">
+
+<param name="quality" value="high" />
+
+<param name="src" value="web_flash/button11.swf" /><embed type="application/x-shockwave-flash" width="123" height="30" src="web_flash/button11.swf" quality="high"></embed>
+
+</object>
+
+		</td>
+		<td class="auto-style2"><a href="contactanos.php"><img src="web_images/imagen1.png" border="0" alt="SIEYE" width="57" height="56" /></a><br />
+		<br /><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="139" height="29" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0">
+
+<param name="BGCOLOR" />
+
+<param name="quality" value="high" />
+
+<param name="src" value="web_flash/button10.swf" /><embed type="application/x-shockwave-flash" width="139" height="29" src="web_flash/button10.swf" quality="high"></embed>
+
+</object>
+
+		</td>
+	</tr>
+	
+	
+	</table>
+<table align="center" style="width: 80%" bgcolor="#FFFFFF" class="auto-style3">
+	<tr>
+		<td class="auto-style2" style="height: 23px"></td>
+		<td class="auto-style2" style="height: 23px"></td>
+	</tr>
+</table>
+<table align="center" style="width: 80%" bgcolor="#FFFFFF">
+	
+	
+	<tr>
+		<td class="auto-style2" style="height: 148px">
+
+		<p align="center" class="slogan Estilo2 Estilo5 Estilo1"><font face="Script MT Bold">Atencion&nbsp;y&nbsp;Servicio&nbsp;de&nbsp;Energia&nbsp;en&nbsp;Emergencia</font></p>
+<p align="center" class="Estilo4 Estilo1 Estilo2">Panuco N&ordm; 5 </p>
+<p align="center" class="Estilo4 Estilo1 Estilo2">Col. Ignacio Lopez Rayon Atizapan Edo. de Mexico C.P 52986</p>
+<p align="center" class="Estilo4 Estilo1 Estilo2">Tel y Fax 55 53 62 30 81&nbsp; Tel. 55 53 62 22 91</p>
+<p align="center" class="Estilo4 Estilo1 Estilo2">Cel.04455 3712 0089</p>
+<p align="center" class="Estilo4 Estilo1"><span class="Estilo2">Correo: <a href="mailto:jmnr14.jmnr@gmail.com">jmnr14.jmnr@gmail.com</a>&nbsp; / <a href="mailto:jmnunor@hotmail.com">jmnunor@hotmail.com</a></span></p>
+<p align="center"><br />
+</p>
+<p align="center">  
+<?php if (!isset($_POST['email'])) { ?>   
+  
+<form action="<?=$_SERVER['PHP_SELF']?>" method="post">   
+<h1 class="Estilo3">Formulario de contacto</h1>   
+<p class="Estilo3">Nombre y/o empresa:</p>   
+<p><input name="nombre" type="text" id="nombre" onblur="MM_validateForm('nombre','','R');return document.MM_returnValue" />
+</p>   
+<p class="Estilo3">Tel�fono:</p>   
+<p><input name="telefono" type="text" id="telefono" onblur="MM_validateForm('telefono','','RisNum');return document.MM_returnValue" />
+</p>   
+<p class="Estilo3">Email:</p>   
+<p><input name="email" type="text" id="email" onblur="MM_validateForm('email','','NisEmail');return document.MM_returnValue" />
+</p>   
+<p class="Estilo3">Mensaje:</p>   
+<p><textarea name="mensaje" cols="50" rows="6" id="mensaje" onblur="MM_validateForm('mensaje','','R');return document.MM_returnValue"></textarea>
+</p>   
+<p><input type="reset" value="borrar" /> <input type="submit" value="enviar" /></p>   
+</form>     
+<?php }else{   
+  
+$msg= "";   
+$msg= "Mensaje del formulario de contacto de SIEYE.netii.net";   
+$msg.= "\nNombre: ". $_POST['nombre'];   
+$msg.= "\nEmail: ".$_POST['email'];   
+$msg.= "\nTelefono: ". $_POST['telefono'];   
+$msg.= "\nMensaje: \n".$_POST['mensaje'];   
+$remitente = $_POST['email'];   
+$subject = "Mensaje enviado desde SIEYE.netii.net por: ".$_POST['nombre'];   
+mail('n_uts@hotmail.com,jmnunosieye@axtel.net,jmnunor@hotmail.com,ytray@hotmail.es', $subject, $msg, "FROM: $remitente");   
+  ?>   
+<p class="Estilo3"><strong>Mensaje enviado.</strong><br />   
+Su mensaje se ha enviado correctamente, gracias por ponerse en contacto con nosotros</p>   
+<? } ?>&nbsp;</p>
+</div>
+</td>
+	</tr>
+	
+	
+</table>
+
+
+</body>
+
+</html>
